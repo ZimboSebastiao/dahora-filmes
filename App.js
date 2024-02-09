04a3eb4dfbf600d266e8d2f528a237a422dc44ff
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-02-09 08:25:31 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,89 @@
+import {
+  Button,
+  Image,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import logo from "./assets/images/logo.png";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Image source={logo} style={estilos.logo} />
+          <Text>Dá Hora Filmes</Text>
+        </View>
+
+        <View style={estilos.viewBotoes}>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textoBotao}>
+              <Ionicons name="search" size={12} color="white" />
+              Buscar Filmes
+            </Text>
+          </Pressable>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textoBotao}>
+              <Ionicons name="star" size={12} color="gold" />
+              Favoritos
+            </Text>
+          </Pressable>
+        </View>
+
+        <View style={estilos.viewRodape}>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewLogo: {
+    flex: 3,
+    width: "80%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 128,
+    height: 128,
+  },
+  viewBotoes: {
+    flex: 2,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    width: "80%",
+    justifyContent: "space-evenly",
+  },
+  botao: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    padding: 16,
+    backgroundColor: "#5451a6",
+  },
+  textoBotao: {
+    color: "#fff",
+  },
+  viewRodape: {
+    width: "80%",
+    backgroundColor: "red",
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
