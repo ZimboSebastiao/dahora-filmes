f64a32bf775ef5858dbcc2d3436f1d4fa6afc69b
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-02-26 09:25:43 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import SafeContainer from "./src/components/SafeContainer";
-import Home from "./src/screens/Home";
 import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
+
+// Criação/inicialização do mecanismo Stack
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Home /> */}
-      {/* <Sobre /> */}
-      <Privacidade />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Sobre" component={Sobre} />
+          <Stack.Screen name="Privacidade" component={Privacidade} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
