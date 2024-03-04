1466eb6db91fb935a94b14eef64ea494fdbde6e9
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-03-04 11:18:39 -0300

    Update api-movidb.js

--- src/services/api-movidb.js
+++ src/services/api-movidb.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
 // Usamos o axios para criar uma referência ao endpoint da API/Service (chamada de baseURL) e configuramos também o uso da API key
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export { api, apiKey };
