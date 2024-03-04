4f16ef4b926b7b6dce713c30976370cbabfe1a2a
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-03-04 11:35:22 -0300

    Update Resultados.js

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,12 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SafeContainer from "../components/SafeContainer";
+import { api, apiKey } from "../services/api-movidb";
+import { useState, useEffect } from "react";
 
 // Prop route
 // Prop especial e definida pelo react Navigation. Através dela é possivel acessar valores passados por meio de navegação entre elas
 export default function Resultados({ route }) {
+  // State para gerenciar os resultados da busca na API
+  const [resultados, setResultados] = useState([]);
+
   // capturando o parâmetro filme vindo de BuscarFilmes
   const { filme } = route.params;
+
+  useEffect(() => {
+    async function BuscarFilmes() {
+      try {
+        const resposta = await api.get("");
+      } catch (error) {}
+    }
+  }, []);
+
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
